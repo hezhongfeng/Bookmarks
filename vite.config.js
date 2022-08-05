f6e7f736da97d18e0b9c9b45493543e05c32b4ff
hezhongfeng <287645587@qq.com>
2022-08-05 16:53:43 +0800

    :sparkles: feat(custom): 项目初始化,多入口项目

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        options: resolve(__dirname, 'src/options/index.html')
+      }
+    }
+  }
+});
