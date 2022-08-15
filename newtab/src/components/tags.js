60ce61332ac91b06adecde13b196adf7b4af8596
hezhongfeng <287645587@qq.com>
2022-08-15 09:15:52 +0800

    :zap: perf(custom): 优化存储结构

--- newtab/src/components/tags.js
+++ newtab/src/components/tags.js
@@ -1,66 +1,68 @@
 import { ref, onMounted } from 'vue';
 
+const tags = ref([]);
+const nodeandtags = ref([]);
+
+const listenUpdateTagMessage = async () => {
+  chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    const { action, payload } = request;
+
+    switch (action) {
+      case 'tags update':
+        console.log('tags update', payload);
+        await getTags();
+        break;
+    }
+    sendResponse();
+  });
+};
+
+const sendUpdateTagMessage = async () => {
+  await chrome.runtime.sendMessage({
+    payload: {},
+    action: 'tags update'
+  });
+};
+
+const createTag = async tag => {
+  if (tags.value.some(item => item === tag)) {
+    return;
+  }
+  tags.value.push(tag);
+  await chrome.storage.sync.set({
+    TAGS: tags.value.map(item => item)
+  });
+  // sendUpdateTagMessage();
+};
+
+const bindTag = async (tag, nodeId) => {
+  if (nodeandtags.value.some(item => item.tag === tag && item.nodeId === nodeId)) {
+    return;
+  }
+  nodeandtags.value.push({
+    tag,
+    nodeId
+  });
+  await chrome.storage.sync.set({
+    NODEANDTAGS: nodeandtags.value.map(item => {
+      return {
+        tag: item.tag,
+        nodeId: item.nodeId
+      };
+    })
+  });
+};
+
+const getTags = async () => {
+  let storageObj = await chrome.storage.sync.get('TAGS');
+  tags.value = 'TAGS' in storageObj ? storageObj.TAGS : [];
+};
+
 export const useTags = () => {
-  const tags = ref([]);
-  const nodeandtags = ref([]);
-
-  const listenUpdateTagMessage = async () => {
-    chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-      const { action, payload } = request;
-
-      switch (action) {
-        case 'update tag':
-          console.log('update tag', payload);
-          await getTags();
-          break;
-      }
-      sendResponse();
-    });
-  };
-
-  const sendUpdateTagMessage = async () => {
-    await chrome.runtime.sendMessage({
-      payload: {},
-      action: 'tags update'
-    });
-  };
-
-  const createTag = async tag => {
-    if (tags.value.some(item => item === tag)) {
-      return;
-    }
-    tags.value.push(tag);
-    console.log('tags', {
-      TAGS: tags
-    });
-    await chrome.storage.sync.set({
-      TAGS: tags
-    });
-    sendUpdateTagMessage();
-  };
-
-  const bindTag = async (tag, nodeId) => {
-    if (nodeandtags.value.some(item => item.tag === tag && item.nodeId === nodeId)) {
-      return;
-    }
-    nodeandtags.value.push({
-      tag,
-      nodeId
-    });
-    await chrome.storage.sync.set({
-      NODEANDTAGS: nodeandtags
-    });
-  };
-
-  const getTags = async () => {
-    let storageObj = await chrome.storage.sync.get('TAGS');
-    tags.value = 'TAGS' in storageObj ? storageObj.TAGS : [];
-  };
-
   onMounted(() => {
+    console.log(62, onMounted);
     listenUpdateTagMessage();
     getTags();
   });
-
   return { tags, createTag, bindTag, sendUpdateTagMessage, getTags };
 };
